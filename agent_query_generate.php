41e81ad71f618b670783a148c369bab20395c811
Pappu Chauhan <pappuchauhan68@gmail.com>
2024-06-04 23:50:45 +0530

    fff

--- agent_query_generate.php
+++ agent_query_generate.php
@@ -20,17 +20,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $save_data["transport"] = json_encode($data_to_store['transport'] ?? []);
   $save_data["permit"] = $data_to_store['permit'] ?? "off";
   $save_data["guide"] = $data_to_store['guide'] ?? "off";
-  $db = getDbInstance(); 
-$db->orderBy('id', 'desc');
-$booking_last = $db->getOne("agent_queries");
-//print_r($booking_last);
-if ($booking_last) {
-  $save_data['booking_code'] = sprintf("TA%04d", $booking_last['id'] + 1);
-} else {
-  $save_data['booking_code'] = sprintf("TA%04d",  1);
-}
-$inserted_id = $db->insert('agent_queries', $save_data);
-
+  $db = getDbInstance();
+  $db->orderBy('id', 'desc');
+  $booking_last = $db->getOne("agent_queries");
+  //print_r($booking_last);
+  if ($booking_last) {
+    $save_data['booking_code'] = sprintf("TA%04d", $booking_last['id'] + 1);
+  } else {
+    $save_data['booking_code'] = sprintf("TA%04d",  1);
+  }
+  $inserted_id = $db->insert('agent_queries', $save_data);
 }
 
 
@@ -59,9 +58,9 @@ $json_vehicle = json_encode($vehicleData);
                     <label class="form-label">Guest Name</label>
                     <input type="text" class="form-control" name="name" placeholder="">
                   </div>
-                
 
-               
+
+
                   <div class="col-md">
                     <label class="form-label">Select Duration</label>
                     <div class="input-group">
@@ -119,7 +118,7 @@ $json_vehicle = json_encode($vehicleData);
                       <thead class="table-dark">
                         <tr>
                           <th class="text-white">Select Transport</th>
-                          
+
                           <th class="text-white">Remarks</th>
                         </tr>
                       </thead>
@@ -133,7 +132,7 @@ $json_vehicle = json_encode($vehicleData);
                               <?php endforeach; ?>
                             </select>
                           </td>
-                       
+
                           <td>Maximum <span class="max-persons"></span> Persons</td>
                         </tr>
                         <tr>
@@ -155,7 +154,7 @@ $json_vehicle = json_encode($vehicleData);
                 </div>
 
                 <div class="row mb-3">
-                   <div class="table-responsive" id="service-per-service">
+                  <div class="table-responsive" id="service-per-service">
 
                   </div>
                 </div>
@@ -163,28 +162,28 @@ $json_vehicle = json_encode($vehicleData);
                 <div class="row mb-3 align-items-top" style="display:none">
                   <div class="col-md-3">
                     <div class="form-check mt-b">
-                      <input class="form-check-input" type="checkbox"  id="bike">
+                      <input class="form-check-input" type="checkbox" id="bike">
                       <label class="form-check-label" for="bike">Bike </label>
                     </div>
                   </div>
                   <div class="col-md-9">
                     <div class="row">
- 
+
 
                     </div>
                   </div>
                 </div>
-                <h3 class="mt-3 mb-3"  id="enter-bike-details">Enter Bike Details</h3>
+                <h3 class="mt-3 mb-3" id="enter-bike-details">Enter Bike Details</h3>
                 <div class="row mb-3" id="enter-bike-details-section">
                   <div class="table-responsive">
                     <table class="table table-bordered">
-                      <tbody class="table-border-bottom-0">   
-                      <tr>
+                      <tbody class="table-border-bottom-0">
+                        <tr>
                           <td>No. of Bike</td>
                           <td colspan="2">
-                             <input type="number" name="number_of_bike" onChange="return calculateTotal();" class="form-control phone-mask">
+                            <input type="number" name="number_of_bike" onChange="return calculateTotal();" class="form-control phone-mask">
                           </td>
-                        </tr>                       
+                        </tr>
                         <tr>
                           <td>Mechanic</td>
                           <td colspan="2">
@@ -333,7 +332,7 @@ $json_vehicle = json_encode($vehicleData);
               </div>
             </div>
             <div class="right-part" style="position:fixed;right:0px">
-              <div class="booking-summary" >
+              <div class="booking-summary">
                 <div class="card">
                   <div class="head">
                     <h3>Booking Summary</h3>
@@ -354,8 +353,8 @@ $json_vehicle = json_encode($vehicleData);
                     <div class="row mb-3">
                       <div class="col-md"><strong>Calculated Price:</strong></div>
                       <div class="col-md"><strong id="summary-calculated-price"></strong></div>
-		    </div>
-		    <div class="row">
+                    </div>
+                    <div class="row">
                       <div class="col-md"><strong>Per Person Price:</strong></div>
                       <div class="col-md"><strong id="per-person-calculated-price"></strong></div>
                     </div>
@@ -458,10 +457,10 @@ $json_vehicle = json_encode($vehicleData);
           days: days,
           tour_date: tour_date
         },
-        success: function(data) { 
-        let dataArr = data.split("break")
-        $('#service-list').html(dataArr[0]);
-        $('#service-per-service').html(dataArr[1]);
+        success: function(data) {
+          let dataArr = data.split("break")
+          $('#service-list').html(dataArr[0]);
+          $('#service-per-service').html(dataArr[1]);
         },
         error: function(xhr, status, error) {
           console.error('Error:', error);
@@ -544,7 +543,7 @@ $json_vehicle = json_encode($vehicleData);
   document.addEventListener('DOMContentLoaded', function() {
     // Function to update maximum number of persons
     function updateMaxPersons() {
-     // console.log( $(this))
+      // console.log( $(this))
       const selectedTransport = $(this).find('option:selected').data('trans');
       $(this).closest('.transport-row').find('.max-persons').text(selectedTransport);
       $(this).closest('.transport-row').find('.num-persons-select').empty();
@@ -553,12 +552,12 @@ $json_vehicle = json_encode($vehicleData);
       }
     }
 
-    function removeAllBelowElement(){
+    function removeAllBelowElement() {
       $('.transport-row:not(:first)').remove();
     }
 
     // Initial setup for first row
-   // $('.transportation-select').each(updateMaxPersons);
+    // $('.transportation-select').each(updateMaxPersons);
 
     // Add more transportation option
     $('#addMoreTransport').click(function(e) {
@@ -575,21 +574,21 @@ $json_vehicle = json_encode($vehicleData);
     $('.table').on('change', '.transportation-select', updateMaxPersons);
     $('.table').on('change', '.transportation-select:first', removeAllBelowElement);
 
-  //bike section hide and show
-  const bikeCheckbox = document.getElementById('bike'); 
-  const bikeDetailsSection = document.getElementById('enter-bike-details-section'); 
-  const bikeDetails = document.getElementById('enter-bike-details'); 
-  bikeDetailsSection.style.display = 'none'; 
-  bikeDetails.style.display = 'none';
-  bikeCheckbox.addEventListener('change', function() { 
-    if (this.checked) { 
+    //bike section hide and show
+    const bikeCheckbox = document.getElementById('bike');
+    const bikeDetailsSection = document.getElementById('enter-bike-details-section');
+    const bikeDetails = document.getElementById('enter-bike-details');
+    bikeDetailsSection.style.display = 'none';
+    bikeDetails.style.display = 'none';
+    bikeCheckbox.addEventListener('change', function() {
+      if (this.checked) {
         bikeDetailsSection.style.display = 'block';
         bikeDetails.style.display = 'block';
-    } else { 
+      } else {
         bikeDetailsSection.style.display = 'none';
         bikeDetails.style.display = 'none';
-    }
-  });
+      }
+    });
 
   });
 
@@ -597,7 +596,7 @@ $json_vehicle = json_encode($vehicleData);
     const packageDetails = document.querySelectorAll('#package-other-details .col-md');
     const targetTableBody = document.querySelector('#final_quotation tbody');
 
-    let totalAmount = 0; 
+    let totalAmount = 0;
     let total_per_person = 0;
     const existingRows = targetTableBody.querySelectorAll('tr:not(:first-child)');
     existingRows.forEach(row => row.remove());
@@ -649,20 +648,20 @@ $json_vehicle = json_encode($vehicleData);
     let totalPax = rowData.totalMember;
     let permitElement = document.getElementById("permit");
     if (permitElement) {
-    if (permitElement.checked) {
-	    permit_amount = permitElement.getAttribute("data-permit");
-	    permit_amount = parseInt(permit_amount*totalPax);
-      total_per_person = total_per_person + parseInt((permit_amount / totalPax));
+      if (permitElement.checked) {
+        permit_amount = permitElement.getAttribute("data-permit");
+        permit_amount = parseInt(permit_amount * totalPax);
+        total_per_person = total_per_person + parseInt((permit_amount / totalPax));
+      }
     }
-  }
     //guide 
     let guideElement = document.getElementById("guide");
     if (guideElement) {
-    if (guideElement.checked) {
-      guide_amount = guideElement.getAttribute("data-guide");
-      total_per_person = total_per_person + parseInt((guide_amount / totalPax));
+      if (guideElement.checked) {
+        guide_amount = guideElement.getAttribute("data-guide");
+        total_per_person = total_per_person + parseInt((guide_amount / totalPax));
+      }
     }
-  }
 
     //services
     const serviceDetails = {};
@@ -705,24 +704,24 @@ $json_vehicle = json_encode($vehicleData);
           total_per_person = total_per_person + ((amount * quantity) / totalPax);
         } else if (type == "Per Person") {
           total_per_person = total_per_person + (amount * quantity);
-        } 
+        }
 
       }
     }
 
 
- // Service Per Service
-const perService = document.getElementById('service-per-service');  
-perService.querySelectorAll('.row.mb-3.align-items-top').forEach(row => { 
-    const input = row.querySelector('input[type="number"]'); 
-    const label = row.querySelector('.form-check-label').textContent.trim();
-    const amount = input.getAttribute('amount-per-service')
-    const quantity = parseInt(input.value) || 0; 
-    if(quantity>0){
-      total_per_person = total_per_person + ((amount * quantity)/totalPax);
-    }  
-});
- 
+    // Service Per Service
+    const perService = document.getElementById('service-per-service');
+    perService.querySelectorAll('.row.mb-3.align-items-top').forEach(row => {
+      const input = row.querySelector('input[type="number"]');
+      const label = row.querySelector('.form-check-label').textContent.trim();
+      const amount = input.getAttribute('amount-per-service')
+      const quantity = parseInt(input.value) || 0;
+      if (quantity > 0) {
+        total_per_person = total_per_person + ((amount * quantity) / totalPax);
+      }
+    });
+
 
     //Transportation 
     const driverTableBody = document.querySelector('#driver_list tbody');
@@ -761,21 +760,21 @@ perService.querySelectorAll('.row.mb-3.align-items-top').forEach(row => {
     });
 
 
-// Get Bike Details
-const numberOfBike = document.querySelector('input[name="number_of_bike"]').value;
-const mechanic = document.querySelector('select[name="mechanic"]').value;
-const marshal = document.querySelector('select[name="marshal"]').value;
-const fuel = document.querySelector('select[name="fuel"]').value;
-const backup = document.querySelector('select[name="backup"]').value;
+    // Get Bike Details
+    const numberOfBike = document.querySelector('input[name="number_of_bike"]').value;
+    const mechanic = document.querySelector('select[name="mechanic"]').value;
+    const marshal = document.querySelector('select[name="marshal"]').value;
+    const fuel = document.querySelector('select[name="fuel"]').value;
+    const backup = document.querySelector('select[name="backup"]').value;
 
-// Retrieve the values of the inputs
-const numberOfBikePrice = parseInt(numberOfBike) * parseInt(<?php echo Bike ?>);
-const mechanicPrice = mechanic == 'Yes'? parseInt(<?php echo Mechanic ?>) : 0;
-const marshalPrice = marshal == 'Yes'? parseInt(<?php echo Marshal ?>) : 0;
-const fuelPrice = fuel == 'Yes'? parseInt(<?php echo Fuel ?>) : 0;
-const backupPrice = backup == 'Yes'? parseInt(<?php echo Backup ?>) : 0;
-//document.getElementById('bike').checked 
-const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + fuelPrice + backupPrice)
+    // Retrieve the values of the inputs
+    const numberOfBikePrice = parseInt(numberOfBike) * parseInt(<?php echo Bike ?>);
+    const mechanicPrice = mechanic == 'Yes' ? parseInt(<?php echo Mechanic ?>) : 0;
+    const marshalPrice = marshal == 'Yes' ? parseInt(<?php echo Marshal ?>) : 0;
+    const fuelPrice = fuel == 'Yes' ? parseInt(<?php echo Fuel ?>) : 0;
+    const backupPrice = backup == 'Yes' ? parseInt(<?php echo Backup ?>) : 0;
+    //document.getElementById('bike').checked 
+    const total_bike_price = (numberOfBikePrice + mechanicPrice + marshalPrice + fuelPrice + backupPrice)
 
     rowData.items.forEach(function(item) {
       let price = item.price + total_per_person;
@@ -786,17 +785,17 @@ const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + f
         price = item.price + (total_per_person * 2)
         h_pax = item.quantity * 2;
         total = price * item.quantity;
-        per_person_pr = (total/ h_pax);
+        per_person_pr = (total / h_pax);
       } else if ("TRIPLE" == item.label.trim()) {
         price = item.price + (total_per_person * 3)
         h_pax = item.quantity * 3;
         total = price * item.quantity;
-        per_person_pr = (total/ h_pax);
+        per_person_pr = (total / h_pax);
       } else if ("QUAD SHARING" == item.label.trim()) {
         price = item.price + (total_per_person * 4)
         h_pax = item.quantity * 4;
         total = price * item.quantity;
-        per_person_pr = (total/ h_pax);
+        per_person_pr = (total / h_pax);
       }
       const newRow = document.createElement('tr');
       newRow.innerHTML = `
@@ -808,9 +807,9 @@ const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + f
       targetTableBody.appendChild(newRow);
       totalAmount += total;
     });
-//totalMember
-    if(document.getElementById('bike').checked ){
-    const newRow = document.createElement('tr');
+    //totalMember
+    if (document.getElementById('bike').checked) {
+      const newRow = document.createElement('tr');
       newRow.innerHTML = `
                 <td>Bike</td>
                 <td>${(total_bike_price/ numberOfBike).toFixed(2)}</td>
@@ -820,7 +819,7 @@ const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + f
       targetTableBody.appendChild(newRow);
       totalAmount += total_bike_price;
     }
-   
+
     // Hotel List
     /*
     const hotelList = document.getElementById('hotel-list');
@@ -849,7 +848,7 @@ const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + f
     var finalPrice = totalAmount;
     var igstPrice = round((totalAmount * 0.025));
     var sgstPrice = round((totalAmount * 0.025));
-       finalPrice = round((finalPrice + igstPrice + sgstPrice));
+    finalPrice = round((finalPrice + igstPrice + sgstPrice));
     const totalRows = `
         <tr>
             <td></td>
@@ -875,15 +874,15 @@ const total_bike_price =  (numberOfBikePrice +  mechanicPrice + marshalPrice + f
 
     targetTableBody.insertAdjacentHTML('beforeend', totalRows);
 
-    document.getElementById('summary-duration').innerHTML = document.getElementById('duration').value ;
+    document.getElementById('summary-duration').innerHTML = document.getElementById('duration').value;
     document.getElementById('summary-travel-date').innerHTML = $('input[name="tour_start_date"]').val();
     document.getElementById('summary-no-of-pax').innerHTML = totalPax;
-    document.getElementById('summary-calculated-price').innerHTML = '₹'+finalPrice;
-    document.getElementById('per-person-calculated-price').innerHTML = '₹'+round(parseInt(finalPrice/totalPax));
+    document.getElementById('summary-calculated-price').innerHTML = '₹' + finalPrice;
+    document.getElementById('per-person-calculated-price').innerHTML = '₹' + round(parseInt(finalPrice / totalPax));
   }
 
   function round(number) {
-    return Math.round(number * 100) / 100; 
+    return Math.round(number * 100) / 100;
   }
 
 
